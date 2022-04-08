03ee5d45424d2a5cecd695f154957257f0649c4c
Vyacheslav Serdyuk <Navarachkou@gmail.com>
2022-04-08 16:58:25 +0500

    init: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ChangeEvent, FC, useEffect, useState } from 'react';
 import './App.css';
+import TodoTask from './Components/TodoTask';
+import { ITask } from './Interfaces';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: FC = () => {
+    const [task, setTask] = useState<string>('');
+    const [deadline, setDeadline] = useState<number>(0);
+    const [todoList, setTodoList] = useState<ITask[]>(() => {
+        const saved = localStorage.getItem('state');
+        if (!saved) return [];
+        return JSON.parse(saved);
+    });
+
+    const saveState = (state: ITask[]): void => {
+        localStorage.setItem('state', JSON.stringify(state));
+    };
+
+    useEffect(() => {
+        saveState(todoList);
+    }, [todoList]);
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+        if (event.target.name === 'task') {
+            setTask(event.target.value);
+        } else {
+            setDeadline(Number(event.target.value));
+        }
+    };
+
+    const addTask = (): void => {
+        const newTask = {
+            taskName: task,
+            deadline: deadline,
+            number: new Date().valueOf(),
+        };
+        setTodoList([...todoList, newTask]);
+        setTask('');
+        setDeadline(0);
+    };
+
+    const completeTask = (taskIdToDelete: number): void => {
+        setTodoList(
+            todoList.filter((task) => {
+                return task.number !== taskIdToDelete;
+            })
+        );
+    };
+
+    return (
+        <div className="App">
+            <div className="header">
+                <input
+                    type="text"
+                    value={task}
+                    name="task"
+                    placeholder="Task"
+                    onChange={handleChange}
+                />
+                <input
+                    type="number"
+                    value={deadline}
+                    name="deadline"
+                    placeholder="Deadline"
+                    onChange={handleChange}
+                />
+                <button onClick={addTask}>Add Task</button>
+            </div>
+            <div className="todo-list">
+                {todoList.map((task: ITask) => {
+                    return (
+                        <TodoTask
+                            key={task.number}
+                            task={task}
+                            completeTask={completeTask}
+                        />
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/Components/TodoTask.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ITask } from '../Interfaces';
+
+interface Props {
+    task: ITask;
+    completeTask(taskIdToDelete: number): void;
+}
+
+const TodoTask = ({task, completeTask}: Props) => {
+    return (
+        <div className="task">
+            <div className="content">
+                <span>{task.taskName}</span>
+                <span>{task.deadline}</span>
+            </div>
+            <button onClick={() => completeTask(task.number)}>X</button>
+        </div>
+    )
+};
+
+export default TodoTask;
